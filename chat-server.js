e666f7067e46f77a9906b01c740e5a477a98ba71
Mirhad Osmanovic <mirhadosmanovic@wustl.edu>
2017-03-31 16:00:29 -0500

    beta version, socket not emitting

--- chat-server.js
+++ chat-server.js
@@ -3,7 +3,14 @@ var http = require("http");
 var socketio = require("socket.io");
 var fs = require("fs");
 var people = {};
- 
+var rooms = {};
+rooms["CSE330"] = {
+				"admin": "",
+				"users": [],
+				"pub": "true",
+				"pass": "",
+				"banned": []
+		};
 // Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
 var app = http.createServer(function(req, resp){
 	// This callback runs when a new connection is made to our HTTP server.
@@ -22,11 +29,57 @@ var io = socketio.listen(app);
 io.sockets.on("connection", function(socket){
 	// This callback runs when a new Socket.IO connection is established.
 	
-	 socket.on("join", function(name){
-        people[socket.id] = name;
-        socket.emit("update", "You have connected to the server.");
-        io.sockets.emit("update", name + " has joined the server.");
-        io.sockets.emit("update-people", people);
+	 socket.on("submitusername", function(name){
+        people[socket.id] = { "name" : name, "room" : ""};
+		socket.emit("loadpage", rooms);
+    });
+
+	 socket.on("join", function(room, pub, pass){
+		
+		var banned = false;
+		for (i = 0; i < rooms[room].banned.length; ++i){
+			if (people[socket.id].name == rooms[room].banned[i]){
+				banned = true;
+			}
+		}
+		
+		if(banned){
+			socket.emit("banned");
+		}else{
+		
+		
+		if (pub === "false"){
+			if (rooms[room].pass == pass){
+				people[socket.id].room = room;
+				rooms[room].users.push(socket.id);
+				io.sockets.emit("update", rooms[room].users);
+			} else {
+				socket.emit("wrongpassword");
+			}
+		}else{
+
+				people[socket.id].room = room;
+				rooms[room].users.push(socket.id);
+				io.sockets.emit("update", rooms[room].users);
+			
+		}
+		}
+		
+    });
+	 
+	  socket.on("createroom", function(data){
+        var roomAdmin = people[socket.id];
+		
+		rooms[data.roomname] = {
+				"admin": roomAdmin,
+				"users": [roomAdmin],
+				"pub": data.pub,
+				"pass": data.password,
+				"banned": []
+		};
+		people[socket.id].room = roomname;
+ console.log("about to emit update");
+        socket.emit("update", rooms[roomname].users);
     });
 	
 //	socket.on("disconnect", function(){
@@ -37,7 +90,12 @@ io.sockets.on("connection", function(socket){
 	
 	socket.on('message_to_server', function(msg) {
 		// This callback runs when the server receives a new message from the client.
-
-		io.sockets.emit("message_to_client", people[socket.id], msg); // broadcast the message to other users
+		var chatroom = people[socket.id].room;
+		var allusersinchatroom = rooms[chatroom].users;
+		
+		for (i = 0; i < allusersinchatroom.length; ++i) {
+			io.to(allusersinchatroom[i]).emit("message_to_client", people[socket.id].name, msg); // broadcast the message to other users
+		}
+		
 	});
 });
